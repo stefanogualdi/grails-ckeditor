fe74faa1a017cbb2a76fc014e878eab4adf8aa4d
Stefano Gualdi <stefano.gualdi@gmail.com>
2010-06-24 13:31:58 +0200

    Updated plugin's version

--- CkeditorGrailsPlugin.groovy
+++ CkeditorGrailsPlugin.groovy
@@ -18,7 +18,7 @@
 
 class CkeditorGrailsPlugin {
     // the plugin version
-    def version = "0.5"
+    def version = "0.6"
 
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.1 > *"
