cf7e1e9f11955e1dbf61e0968fbb3b34d572e2c6
Stefano Gualdi <stefano.gualdi@gmail.com>
2010-10-08 23:57:55 +0200

    Version bump

--- CkeditorGrailsPlugin.groovy
+++ CkeditorGrailsPlugin.groovy
@@ -18,7 +18,7 @@
 
 class CkeditorGrailsPlugin {
     // the plugin version
-    def version = "3.4.1.0"
+    def version = "3.4.1.1-SNAPSHOT"
 
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.1 > *"
