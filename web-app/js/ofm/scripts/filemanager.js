f5e26b82930036b619b437882dd1a327fa9e2fd7
Stefano Gualdi <stefano.gualdi@gmail.com>
2011-02-22 20:30:25 +0100

    Little vertical adjustment to avoid vertical scroll bar on main window

--- web-app/js/ofm/scripts/filemanager.js
+++ web-app/js/ofm/scripts/filemanager.js
@@ -54,7 +54,7 @@ $.prompt.setDefaults({
 // Forces columns to fill the layout vertically.
 // Called on initial page load and on resize.
 var setDimensions = function(){
-	var newH = $(window).height() - $('#uploader').height();	
+	var newH = $(window).height() - $('#uploader').height() - 30;
 	$('#splitter, #filetree, #fileinfo, .vsplitbar').height(newH);
 }
 
