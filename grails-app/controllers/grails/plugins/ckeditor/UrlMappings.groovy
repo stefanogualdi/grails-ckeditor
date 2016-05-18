e33b40f1933ab7dea905ee8bd73ecbdc88501b33
Ben Lucchesi <benlucchesi@gmail.com>
2016-05-17 17:02:03 -0700

    remove URL mappings

--- grails-app/controllers/grails/plugins/ckeditor/UrlMappings.groovy
+++ grails-app/controllers/grails/plugins/ckeditor/UrlMappings.groovy
@@ -19,14 +19,5 @@ package grails.plugins.ckeditor
 class UrlMappings {
 
     static mappings = {
-        "/$controller/$action?/$id?(.$format)?"{
-            constraints {
-                // apply constraints here
-            }
-        }
-
-        "/"(view:"/index")
-        "500"(view:'/error')
-        "404"(view:'/notFound')
     }
 }
